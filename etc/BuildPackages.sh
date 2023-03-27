ca29d772ca2d6fbdec9839a8691e91c03f051e69
Max Horn <max@quendi.de>
2023-03-27 22:27:01 +0200

    Simplify BuildPackages.sh

--- etc/BuildPackages.sh
+++ etc/BuildPackages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+set -x
 
 GAPROOT="$1"
 PKGDIR="$2"
@@ -10,22 +11,14 @@ PKGDIR="$2"
 # is BSD make, not GNU make.
 if command -v gmake >/dev/null 2>&1
 then
-  MAKE="gmake"
-else
-  MAKE="make"
+  alias make=gmake
 fi
-
-# print the given command plus arguments, then run it
-echo_run() {
-  echo "Running $@"
-  "$@"
-}
 
 cd "$PKGDIR"
 
 if [ -x autogen.sh ] && [ ! -x configure ]
 then
-  echo_run ./autogen.sh
+  ./autogen.sh
 fi
 if [ -x configure ]
 then
@@ -33,18 +26,18 @@ then
   # or not, without actually executing it!
   if grep Autoconf ./configure > /dev/null
   then
-    echo_run ./configure --with-gaproot="$GAPROOT"
+    ./configure --with-gaproot="$GAPROOT"
     # hack: run `make clean` in case the package was built before with different settings
-    echo_run "$MAKE" clean
+    make clean
   else
-    echo_run ./configure "$GAPROOT"
+    ./configure "$GAPROOT"
     # hack: run `make clean` in case the package was built before with different settings
-    echo_run "$MAKE" clean
+    make clean
     # hack: in browse and edim, `make clean` removes `Makefile` so run configure
     # again to ensure we can actually build them (we could restrict this hack to
     # the two offending packages, but since non-autoconf configure is super cheap,
     # there seems little reason to bother)
-    echo_run ./configure "$GAPROOT"
+    ./configure "$GAPROOT"
   fi
-  echo_run "$MAKE"
+  make
 fi
