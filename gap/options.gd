8d3cec9140786de9c56db16e1c5c2c06503a19da
Michael Young <mct25@st-andrews.ac.uk>
2025-04-30 17:35:32 +0100

    Try no-upgrade and upgrade approaches and compare

--- gap/options.gd
+++ gap/options.gd
@@ -3,6 +3,7 @@ DeclareOperation("PKGMAN_Option", [IsString, IsRecord]);
 
 PKGMAN_GlobalOptions := rec(
   distro    := "latest",
+  install   := "ask",
   suggested := false,
   upgrade   := "ask",
   version   := "",
